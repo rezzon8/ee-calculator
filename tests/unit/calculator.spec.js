720918c4ba555b7d9528ea439b52707db9dea584
Clinton Botha <clinton@tangentsolutions.co.za>
2021-01-26 19:10:09 +0200

    add basic unit test specs - tdd

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -2,11 +2,34 @@ import { shallowMount } from "@vue/test-utils";
 import Calculator from "@/components/Calculator.vue";
 
 describe("Calculator.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
+  it("renders props.title when passed", () => {
+    const title = "The Simple Calculator";
     const wrapper = shallowMount(Calculator, {
-      propsData: { msg }
+      propsData: { title }
     });
-    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper.text()).toContain(title);
+  });
+  it("renders props.description when passed", () => {
+    const description = "A simple description";
+    const wrapper = shallowMount(Calculator, {
+      propsData: { description }
+    });
+    expect(wrapper.text()).toContain(description);
+  });
+  it("should add 2 numbers", () => {
+    const wrapper = shallowMount(Calculator, {});
+    console.log(wrapper.html());
+  });
+  it("should subtract one number from another", () => {
+    const wrapper = shallowMount(Calculator, {});
+    console.log(wrapper.html());
+  });
+  it("should multiply 2 numbers together", () => {
+    const wrapper = shallowMount(Calculator, {});
+    console.log(wrapper.html());
+  });
+  it("should divide 1 number by another", () => {
+    const wrapper = shallowMount(Calculator, {});
+    console.log(wrapper.html());
   });
 });
